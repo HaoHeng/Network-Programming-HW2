deea3fdb5017550fa54ce0a51f3b1ecafdb8e47c
HaoHeng <henry199442@gmail.com>
2015-10-18 15:33:13 +0800

    ** environment: change from python3 to python2 ** all transition table complete. todo: display bricks and fix bug: server issued an error after disconnected by client.

--- tcp_client.py
+++ tcp_client.py
@@ -1,55 +1,108 @@
 #!/usr/bin/env python
-from socket import *
+import socket
 from time import ctime
-
+import json
+from io import StringIO
+import pickle
 
 HOST = '127.0.0.1'
 PORT = 5566
 ADDR = (HOST, PORT)
 
+def display_bricks(scenario):
+	print scenario
+	sss=json.loads(scenario)
+	print sss['message']
+	return
 
 def fsm_state_local_transition_table(command):
 	if command=='connect':
 		tcpCliSock.connect(ADDR)
 		return 'server has been connected'
 	else:
-		print("Please connect to server first.")
+		print "Please connect to server first."
 		return 'local'
 
 def fsm_state_server_has_been_connected_transition_table(command):
 	if command=='connect':
-		print("Have already connected to server")
+		print "Have already connected to server"
 		return 'server has been connected'
 	elif command=='disconnect':
-		print("disconnect from game server")
+		print "disconnect from game server"
 		tcpCliSock.close()
 		return 'local'
+	elif command=='new':
+		tcpCliSock.sendall("{'action':'New'}")
+		game_scenario=tcpCliSock.recv(1024)
+		display_bricks(game_scenario)
+		
+		return 'during a game'
+		
 	else:
-		print("Please new a game round first")
+		print "Please new a game round first"
 		return 'server has been connected'
 
 def fsm_state_during_a_game_transition_table(command):
-	return
+	if command=='new':
+		print "Have already in a game round"
+		return 'during a game'
+	elif command=='end':
+		tcpCliSock.sendall('{"action":"End"}')
+		print "The game has closed"
+		return 'server has been connected'
+	elif command=='w':
+		tcpCliSock.sendall('{"action":"moveUp"}')
+		game_scenario=tcpCliSock.recv(1024)
+		display_bricks(game_scenario)
+		return 'during a game'
+	elif command=='a':
+		tcpCliSock.sendall('{"action":"moveRight"}')
+		game_scenario=tcpCliSock.recv(1024)
+		display_bricks(game_scenario)
+		return 'during a game'
+	elif command=='s':
+		tcpCliSock.sendall('{"action":"moveDown"}')
+		game_scenario=tcpCliSock.recv(1024)
+		display_bricks(game_scenario)
+		return 'during a game'
+	
+	elif command=='d':
+		tcpCliSock.sendall('{"action":"moveLeft"}')
+		game_scenario=tcpCliSock.recv(1024)
+		display_bricks(game_scenario)
+		return 'during a game'
+	
+	elif command=='u':
+		tcpCliSock.sendall('{"action":"unDo"}')
+		game_scenario=tcpCliSock.recv(1024)
+		display_bricks(game_scenario)
+		return 'during a game'
+	else:
+		print "wrong command!"
+		return 'during a game'
+	
+		
+		
 
 	
 fsm_state="local"
 
-tcpCliSock = socket(AF_INET, SOCK_STREAM)
+tcpCliSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-print("Welcome to Game 2048!")
-print("enter 'help' to get more information.")
+print "Welcome to Game 2048!"
+print "enter 'help' to get more information."
 
 while True:
 	if fsm_state=='local':
-		user_command = input(">")
+		user_command = raw_input(">")
 		fsm_state=fsm_state_local_transition_table(user_command)
 		
 	elif fsm_state=='server has been connected':
-		user_command = input(">")
+		user_command = raw_input(">")
 		fsm_state=fsm_state_server_has_been_connected_transition_table(user_command)
 		
 	elif fsm_state=='during a game':
-		user_command = input("move>")
+		user_command = raw_input("move>")
 		fsm_state=fsm_state_during_a_game_transition_table(user_command)
 		
 	else:
